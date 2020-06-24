d8513dbae118b026777f7bd8fef1e025dd58a67a
viviana-gherasim <viviana-gherasim@users.noreply.github.com>
2020-06-24 14:40:58 +0300

    Added a toolbar to open/close the navigation drawer.

--- app/src/main/java/com/example/traveljournal/HomeActivity.java
+++ app/src/main/java/com/example/traveljournal/HomeActivity.java
@@ -1,14 +1,34 @@
 package com.example.traveljournal;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToogle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
