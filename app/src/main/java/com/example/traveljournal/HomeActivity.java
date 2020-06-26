602bc57bbce9eb97bf1d8f485f4e47ba76b09d49
viviana-gherasim <viviana-gherasim@users.noreply.github.com>
2020-06-26 14:02:29 +0300

    Tried to change the scene to an xml file.

--- app/src/main/java/com/example/traveljournal/HomeActivity.java
+++ app/src/main/java/com/example/traveljournal/HomeActivity.java
@@ -1,11 +1,19 @@
 package com.example.traveljournal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.net.Network;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -31,4 +39,21 @@ public class HomeActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+   /* public  boolean onNavigationItemSelected(MenuItem item){
+        Fragment fragment = null;
+        int id = item.getItemId();
+        if(id == R.id.home)
+            fragment = new HomeFragment();
+        if(fragment != null){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+
+        }
+    }
+
+    */
 }
+
+
+
